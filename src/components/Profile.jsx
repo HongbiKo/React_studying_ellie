debf6c45d84fe09f6570a10189a0fcbde65bb7c2
HongbiKo <rhghdql666@gmail.com>
2023-10-31 13:51:19 +0900

    adding statement_new-developer

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 
-export default function Profile({image, name, title}) {
+export default function Profile({image, name, title, stateNew}) {
   return (
     <div className="profile">
+      {stateNew ? <span className='new'>new</span> : null}
       <img className="photo" src={image} alt="avatar" />
       <h1>{name}</h1>
       <p>{title}</p>
