566acb405b58f6729901731c934b5f98c27d6503
HongbiKo <rhghdql666@gmail.com>
2023-11-02 16:47:14 +0900

    day04-making sale product's list

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,21 +1,27 @@
 import React, { useEffect, useState } from 'react';
 
 export default function Products() {
-  const [count, setCount] = useState(0);
   const [products, setProducts] = useState([]);
+  const [checked, setChecked] = useState(false);
+  const handleChange = () => setChecked((prev) => !prev);
 
   useEffect(() => {
-    fetch('data/products.json')
+    fetch(`data/${checked ? 'sale_' : ''}products.json`)
       .then((res) => res.json())
       .then((data) => {
-        console.log("업데이트됨");
+        console.log("뜨끈한 데이터를 네트워크에서 받아옴");
         setProducts(data);
       });
-  }, []);
-  
+      return () => {
+        console.log('깨끗하게 청소하는 일들을 합니다.')
+      }
+  }, [checked]);
+
   return (
     <div>
       <>
+        <input id="checkbox" type="checkbox" value={checked} onChange={handleChange}></input>
+        <label htmlFor="checkbox">Show only hot sale</label>
         <ul> 
           {
             products.map((product) => (
@@ -28,7 +34,6 @@ export default function Products() {
             ))
           }
         </ul>
-        <button onClick={() => setCount((prev) => prev + 1)}>{count}</button>
       </>
     </div>
   );
