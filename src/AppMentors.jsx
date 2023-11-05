0a5ebc0d75b0ece76f25fd2d4ccfbeb6dedfd8cb
HongbiKo <rhghdql666@gmail.com>
2023-11-05 21:52:11 +0900

    day05-update, delete, add state of array

--- src/AppMentors.jsx
+++ src/AppMentors.jsx
@@ -1,20 +1,28 @@
 import React, { useState } from 'react';
 
 export default function AppMentors() {
-  const [person, setPerson] = useState({
-    name: '엘리',
-    title: '개발자',
-    mentors: [
-      {
-        name: '밥',
-        title: '시니어개발자'
-      },
-      {
-        name: '제임스',
-        title: '시니어개발자'
+  const [person, setPerson] = useState(initialPerson);
+
+  const handleUpdate = () => {
+    const prevname = prompt(`누구의 이름을 바꾸고 싶은가요?`);
+    const current = prompt(`이름을 무엇으로 바꾸고 싶은가요?`);
+
+    setPerson((prev) => ({...prev, mentors: prev.mentors.map((mentor)=>{
+      if(mentor.name === prevname) {
+        return {...mentor, name: current};
       }
-    ],
-  });
+        return mentor;
+    })}))
+  }
+  const handleAdd = () => {
+    const addName = prompt(`누구의 이름을 추가하고 싶은가요?`);
+        const addTitle = prompt(`직책의 이름은 무엇인가요?`)
+        setPerson((prev)=>({...prev, mentors: prev.mentors.concat({name: addName, title: addTitle}) }))
+  }
+  const handleDelete = () => {
+    const deleteName = prompt(`누구의 이름을 삭제하고 싶은가요?`);
+    setPerson((prev)=> ({...prev, mentors: prev.mentors.filter((mentor)=> mentor.name !== deleteName)}))
+  }
 
   return (
     <div>
@@ -25,22 +33,28 @@ export default function AppMentors() {
           person.mentors.map((mentor, index) => (
             <li key={index}>
               {mentor.name} ({mentor.title})
-          </li>
+            </li>
           ))
         }
       </ul>
-      <button onClick={()=>{
-        const prevname = prompt(`누구의 이름을 바꾸고 싶은가요?`);
-        const current = prompt(`이름을 무엇으로 바꾸고 싶은가요?`);
-
-        setPerson((prev)=>({...prev, mentors: prev.mentors.map((mentor)=>{
-          if(mentor.name === prevname) {
-            return {...mentor, name: current}
-          }
-          return mentor;
-        })}))
-      }}>멘토의 이름을 바꾸기</button>
+      <button onClick={handleUpdate}>멘토의 이름을 바꾸기</button>
+      <button onClick={handleAdd}>멘토 추가</button>
+      <button onClick={handleDelete}>멘토 삭제</button>
     </div>
   );
 }
 
+const initialPerson = {
+  name: '엘리',
+  title: '개발자',
+  mentors: [
+    {
+      name: '밥',
+      title: '시니어개발자'
+    },
+    {
+      name: '제임스',
+      title: '시니어개발자'
+    }
+  ],
+}
